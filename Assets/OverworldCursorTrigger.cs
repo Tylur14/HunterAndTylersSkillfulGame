b06f7a50463aa72b961b3a9d8467672440caa188
Jim <tylursims@gmail.com>
2020-11-14 17:03:53 -0500

    Added Environment Deco
    Added decorations and some minor scripts

---
+++ Assets/OverworldCursorTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class OverworldCursorTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent mouseOverEvents;
+    [SerializeField] private UnityEvent mouseExitEvents;
+
+    private void OnMouseEnter()
+    {
+        mouseOverEvents.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExitEvents.Invoke();
+    }
+}
