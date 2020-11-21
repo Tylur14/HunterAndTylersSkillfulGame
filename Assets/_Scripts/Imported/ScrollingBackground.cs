9fdc0bf3d585677920155a68cbeeb072def66fea
Jim <tylursims@gmail.com>
2020-11-20 20:14:56 -0500

    Added new artwork & parallax effect
    all credit to https://ansimuz.itch.io/

---
+++ Assets/_Scripts/Imported/ScrollingBackground.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	public float scrollSpeed;
+	public float currentSpeed;
+	public float tileWidth;
+
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+		currentSpeed = scrollSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPosition = Mathf.Repeat (Time.time * currentSpeed, tileWidth);
+		transform.position = startPosition + Vector3.left * newPosition;
+	}
+}
